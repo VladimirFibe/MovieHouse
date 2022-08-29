153afc34766d257e8adce03bf65ae6cd88f9aa99
Vladimir <motiw@icloud.com>
2022-08-29 09:03:14 +0600

    Initial Commit

---
+++ MovieHouse/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieHouse
+//
+//  Created by Vladimir Fibe on 29.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
